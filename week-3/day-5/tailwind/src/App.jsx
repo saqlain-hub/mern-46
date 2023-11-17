877c3288706deee8bd7849f77ccccabcb1cd072b
Saqlain Javed <saqlainsj12@gmail.com>
2023-11-18 00:56:35 +0500

    changes to tailwind project

--- week-3/day-5/tailwind/src/App.jsx
+++ week-3/day-5/tailwind/src/App.jsx
@@ -1,4 +1,3 @@
-import "./App.css";
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
--- week-3/day-5/tailwind/src/components/Navbar.jsx
+++ week-3/day-5/tailwind/src/components/Navbar.jsx
@@ -1,13 +1,17 @@
 import React from "react";
-import img from "../assets/linkedin.png";
-import menu from "../assets/menu.png";
+import icon from "../assets/icon.jpg";
 
 const Navbar = () => {
   return (
     <header>
       <nav className="container flex items-center py-4 mt-4 sm:mt-12">
-        <div className="py-1 flex flex-column gap-2 items-center cursor-pointer">
-          <img width={30} src={img} alt="an image" />
+        <div className="py-1 flex flex-column gap-4 items-center cursor-pointer">
+          <img
+            className="rounded-md"
+            width={40}
+            src={icon}
+            alt="an icon image"
+          />
           <span className="uppercase text-lg font-bold">Bookmark</span>
         </div>
         <ul className="hidden sm:flex flex-1 justify-end items-center gap-12 text-bk-blue uppercase text-sm">
@@ -21,8 +25,8 @@ const Navbar = () => {
             Login
           </button>
         </ul>
-        <div className="flex sm:hidden flex-1 justify-end">
-          <img src={menu} alt="menu" width={25} />
+        <div className="flex cursor-pointer sm:hidden flex-1 justify-end">
+          <i className="text-2xl fa-solid fa-bars"></i>
         </div>
       </nav>
     </header>
