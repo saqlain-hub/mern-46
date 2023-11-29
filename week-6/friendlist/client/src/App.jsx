7f282b3c0c2f961b06f4e7c12dbeb2047ee44851
Saqlain Javed <saqlainsj12@gmail.com>
2023-11-29 07:14:59 +0500

    UI prepared half

--- week-6/friendlist/client/src/App.jsx
+++ week-6/friendlist/client/src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="bg-gray-500 rounded-t-md">
+      <h1 className="text-3xl py-2 border-b-2 border-gray-900">
+        FriendList CRUD
+      </h1>
+      <div className="flex justify-between p-12">
+        <section className="flex-1 border-2 border-red-500 py-4 px-6 rounded-md bg-gray-600">
+          <button className="border-2 border-yellow-400 px-4 py-2 rounded-md bg-green-300 text-black">
+            Get Friends List
+          </button>
+
+          <ul className="rounded-md mt-4 border-2 border-gray-300">
+            <h3 className="py-2 border-b-2 border-gray-300 bg-gray-800 rounded-md">
+              Friends List
+            </h3>
+            <li>something</li>
+            <li>something</li>
+            <li>something</li>
+          </ul>
+        </section>
+        <section className="flex-1">
+          <form>
+            <p>
+              <label htmlFor="">Enter Name</label>
+              <input type="text" />
+            </p>
+          </form>
+        </section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
