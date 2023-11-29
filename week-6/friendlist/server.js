d9b60b78c2f9599e9e435ef54b93fa4886e2a394
Saqlain Javed <saqlainsj12@gmail.com>
2023-11-29 06:45:22 +0500

    Friendlist with CRUD functionalities and frontend

--- week-6/friendlist/server.js
+++ week-6/friendlist/server.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
 const app = express();
 
 app.use(express.json());
@@ -7,12 +9,43 @@ const router = express.Router();
 
 app.use(router);
 
-const PORT = 5000;
+dbConn().catch((err) => console.log("Connection error", err));
+async function dbConn() {
+  await mongoose.connect("mongodb://127.0.0.1:27017/friendslist");
 
-router.get("/", (req, res) => {
-  res.json("Server Running");
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+const FriendsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+  contact: {
+    type: Number,
+  },
 });
 
+const FriendsModel = new mongoose.model("FriendModel", FriendsSchema);
+
+router.all("/", (req, res) => {
+  res.json("Server Running :)");
+});
+
+router.get("/friend", async (req, res) => {
+  friendlist = await FriendsModel.find();
+  res.json(friendlist);
+});
+
+router.post("/friend", async (req, res) => {
+  const newFriend = new FriendsModel({ ...req.body });
+  const insertedFriend = await newFriend.save();
+  res.status(200).json(insertedFriend);
+});
+
+const PORT = 5000;
 app.listen(PORT, () => {
   console.log("Server running on port: ", PORT);
 });
