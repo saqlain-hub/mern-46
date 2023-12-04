25dcc8d7efa59d437c663779b1d4a5f405703b10
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-05 01:45:49 +0500

    controllers, database connection, middleware, routes

--- week-6/day-4/app.js
+++ week-6/day-4/app.js
@@ -1,8 +1,16 @@
 const express = require("express");
+const cors = require("cors");
+
+const todoRoutes = require("./routes/todo.routes");
+const connectDatabase = require("./database/connection");
+const handleError = require("./middleware/error");
+
+connectDatabase();
 const app = express();
 
-app.get("/", (req, res) => {
-  res.json("Server Running");
-});
+app.use(cors());
+app.use(express.json());
+app.use("/todo", todoRoutes);
+app.use(handleError);
 
-module.exports = app;
+module.exports = app; // default export
