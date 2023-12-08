04c028789f8b9d1fe5cf225dc38570873eccecd9
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-09 01:33:52 +0500

    nodemailer not working will fix it soon

--- week-7/day-5/mailer/controller/appController.js
+++ week-7/day-5/mailer/controller/appController.js
@@ -1,5 +1,38 @@
-const signup = (req, res) => {
-  res.status(201).json("Signup Successful...!");
+const nodemailer = require("nodemailer");
+
+const signup = async (req, res) => {
+  /** testing account */
+  let testAccount = await nodemailer.createTestAccount();
+
+  // create reusable transporter object using default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "smtp.forwardemail.net",
+    port: 465,
+    secure: true,
+    auth: {
+      user: testAccount.user, /// generated ethereal user
+      pass: testAccount.pass, // generated ethereal password
+    },
+  });
+
+  let message = {
+    from: "Fred Foo <foo@example.com", // sender address
+    to: "saqlainish@pm.me", // list of receivers
+    subject: "Hello", // subject line
+    text: "Hello world is it working", // plain text body
+    html: "<b>Hello World?</b>", // html body
+  };
+
+  transporter.sendMail(message).then(() => {
+    return res
+      .status(201)
+      .json({ msg: "you should receive an email" })
+      .catch((err) => {
+        return res.status(500).json({ err });
+      });
+  });
+
+  // res.status(201).json("Signup Successful...!");
 };
 
 const getBill = (req, res) => {
--- week-7/day-5/mailer/server.js
+++ week-7/day-5/mailer/server.js
@@ -10,5 +10,5 @@ app.use(express.json());
 app.use("/api", appRoute);
 
 app.listen(PORT, () => {
-  console.log("Server has started on", PORT);
+  console.log(`Server is running on https://localhost:${PORT}`);
 });
