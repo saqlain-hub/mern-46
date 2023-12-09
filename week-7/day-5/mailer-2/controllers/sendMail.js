bc39de98d1c41a05f0e0b843f3fb45d7ea88c91d
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-10 00:29:08 +0500

    working on mailer now

--- week-7/day-5/mailer-2/controllers/sendMail.js
+++ week-7/day-5/mailer-2/controllers/sendMail.js
@@ -1,4 +1,8 @@
 const sendMail = async (req, res) => {
+  let testAccount = await nodemailer.createTestAccount();
+
+  let transport = await nodemailer.createTransport({});
+
   res.send("Email Sent");
 };
 
