a1029709092e4792e561a3191a99068cd60c624e
Saqlain Javed <saqlainsj12@gmail.com>
2023-12-17 03:33:19 +0500

    redux toolkit practice

---
+++ week-8/day-1/redux-1-p/src/App.jsx
@@ -0,0 +1,13 @@
+import ComponentOne from "./components/componentOne";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ComponentOne />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ week-8/day-1/redux-1-p/src/components/ComponentTwo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const ComponentTwo = () => {
+  return <div>ComponentTwo</div>;
+};
+
+export default ComponentTwo;
