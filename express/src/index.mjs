6976d2f1f63abc478189d7448257ccffe4c06d25
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-24 06:46:59 +0500

    routes and get method

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -4,6 +4,21 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+app.get("/", (req, res) => {
+  res.status(200).send({ msg: "Hello World" });
+});
+
+app.get("/api/users", (req, res) => {
+  res.send([
+    { id: 1, username: "saqlain", displayName: "saqlain" },
+    { id: 2, username: "shahzad", displayName: "shahzad" },
+  ]);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
+
+// localhost:3000
+// localhost:3000/users
+// localhost:3000/products
