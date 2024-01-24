b54070619dccd996da18fc6e74687fb13f994bdb
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-24 06:59:37 +0500

    params

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -4,15 +4,33 @@ const app = express();
 
 const PORT = process.env.PORT || 3000;
 
+const mockUsers = [
+  { id: 1, username: "saqlain", displayName: "saqlain" },
+  { id: 2, username: "shahzad", displayName: "shahzad" },
+];
+
 app.get("/", (req, res) => {
   res.status(200).send({ msg: "Hello World" });
 });
 
 app.get("/api/users", (req, res) => {
-  res.send([
-    { id: 1, username: "saqlain", displayName: "saqlain" },
-    { id: 2, username: "shahzad", displayName: "shahzad" },
-  ]);
+  res.send(mockUsers);
+});
+
+app.get("/api/users/:id", (req, res) => {
+  console.log(req.params);
+  const parsedId = parseInt(req.params.id);
+  console.log(parsedId);
+  if (isNaN(parsedId)) {
+    return res.status(400).send({ msg: "bad request" });
+  }
+  const findUser = mockUsers.find((user) => user.id === parsedId);
+  if (!findUser) return res.status(404).send({ msg: "User not found" });
+  return res.send(findUser);
+});
+
+app.get("/api/products", (req, res) => {
+  res.send([{ id: 123, name: "chicken breast", price: 240 }]);
 });
 
 app.listen(PORT, () => {
