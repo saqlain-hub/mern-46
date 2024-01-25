c90a78f6070919a33f70454421a2784f92b39df7
Saqlain Javed <saqlainsj12@gmail.com>
2024-01-26 01:55:32 +0500

    DELETE request

--- express/src/index.mjs
+++ express/src/index.mjs
@@ -95,6 +95,25 @@ app.patch("/api/users/:id", (req, res) => {
   return res.sendStatus(200);
 });
 
+// DELETE Request
+app.delete("/api/users/:id", (req, res) => {
+  const {
+    params: { id },
+  } = req;
+
+  const parsedId = parseInt(id);
+
+  if (isNaN(parsedId)) return res.sendStatus(400);
+
+  const findUserIndex = mockUsers.findIndex((user) => user.id === parsedId);
+
+  if (findUserIndex === -1) return res.sendStatus(404);
+
+  mockUsers.splice(findUserIndex, 1);
+
+  return res.sendStatus(200);
+});
+
 app.listen(PORT, () => {
   console.log(`Running on Port ${PORT}`);
 });
