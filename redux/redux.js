6eeeb432be713283db75e6cc8463ed8e199e985a
Saqlain Javed <saqlainsj12@gmail.com>
2024-02-25 10:20:51 +0500

    subscribe

--- redux/redux.js
+++ redux/redux.js
@@ -6,14 +6,15 @@ const store = createStore(reducer);
 // reducer
 function reducer(state = { amount: 1 }, action) {
   if (action.type === "increment") {
+    // immutability
     return { amount: state.amount + 1 };
   }
   return state;
 }
 
 // global state
-console.log(store.getState());
+store.subscribe(() => {
+  console.log(store.getState());
+});
 
 store.dispatch({ type: "increment" });
-
-console.log(store.getState());
