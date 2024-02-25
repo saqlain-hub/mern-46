eab56fdf553651a4d0e10af21e6f559ca730ba5f
Saqlain Javed <saqlainsj12@gmail.com>
2024-02-25 10:18:23 +0500

    createStore, dispatch, getState

--- redux/redux.js
+++ redux/redux.js
@@ -3,6 +3,17 @@ import { createStore } from "redux";
 // store
 const store = createStore(reducer);
 
-function reducer(state, action) {
+// reducer
+function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    return { amount: state.amount + 1 };
+  }
   return state;
 }
+
+// global state
+console.log(store.getState());
+
+store.dispatch({ type: "increment" });
+
+console.log(store.getState());
