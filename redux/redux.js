03be72a7695f4e083a1fce6aa2e82a3c0f9b03eb
Saqlain Javed <saqlainsj12@gmail.com>
2024-04-05 07:16:01 +0500

    revision: (redux and redux-toolkit - state is immutable)

--- redux/redux.js
+++ redux/redux.js
@@ -6,8 +6,16 @@ const store = createStore(reducer);
 // reducer for createStore method
 // a callback function that will be called when createStore() is invoked
 function reducer(state = { amount: 1 }, action) {
+  if (action.type === "increment") {
+    // immutability -- do not mutate state and return a copy
+    return { amount: state.amount + 1 };
+  }
   return state;
 }
 
 // global state
+console.log(store.getState());
+
+store.dispatch({ type: "increment" });
+
 console.log(store.getState());
