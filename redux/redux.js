0ab6dffef9b8f82708347ab91fa50382d2371516
Saqlain Javed <saqlainsj12@gmail.com>
2024-04-05 07:09:14 +0500

    revision: (redux and redux-toolkit with async-thunk, and some concepts)

--- redux/redux.js
+++ redux/redux.js
@@ -3,19 +3,11 @@ import { createStore } from "redux";
 // store
 const store = createStore(reducer);
 
-// reducer
+// reducer for createStore method
+// a callback function that will be called when createStore() is invoked
 function reducer(state = { amount: 1 }, action) {
-  if (action.type === "increment") {
-    // immutability
-    return { amount: state.amount + 1 };
-  }
   return state;
 }
 
 // global state
-store.subscribe(() => {
-  console.log(store.getState());
-});
-
-store.dispatch({ type: "increment" });
-
+console.log(store.getState());
