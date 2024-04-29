9b606b1fd03d4de7ad87d23bcbf8199582989438
Saqlain Javed <saqlainsj12@gmail.com>
2024-04-30 03:01:16 +0500

    Portfolio: (First Section - Navbar)

--- portfolio-project/src/App.jsx
+++ portfolio-project/src/App.jsx
@@ -1,19 +1,21 @@
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
+import Test from "./components/test";
 
 const App = () => {
   return (
-    <div>
-      <section>
-        <Navbar />
-      </section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section>Contact</section>
-    </div>
+    // <div>
+    //   <section>
+    //     <Navbar />
+    //   </section>
+    //   <section>Parallax</section>
+    //   <section>Services</section>
+    //   <section>Portfolio1</section>
+    //   <section>Portfolio2</section>
+    //   <section>Portfolio3</section>
+    //   <section>Contact</section>
+    // </div>
+    <Test />
   );
 };
 
