7b3a747f3c1fe231915de38ad3e4634bb783b2d1
Saqlain Javed <saqlainsj12@gmail.com>
2024-04-30 03:08:48 +0500

    Portfolio: (Playing with animations - Framer Motion)

--- portfolio-project/src/components/test.jsx
+++ portfolio-project/src/components/test.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { motion } from "framer-motion";
 
 const test = () => {
-  return <div>test</div>;
+  return (
+    <div className="course">
+      <motion.div
+        className="box"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 2, delay: 2 }}
+      ></motion.div>
+    </div>
+  );
 };
 
 export default test;
