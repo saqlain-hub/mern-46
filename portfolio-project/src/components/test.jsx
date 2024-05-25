c5506de4a647ad8895518392bb5bd82f1ddcc87c
Saqlain Javed <saqlainsj12@gmail.com>
2024-05-25 18:25:36 +0500

    portfolio project: (new commits)

--- portfolio-project/src/components/test.jsx
+++ portfolio-project/src/components/test.jsx
@@ -1,21 +1,29 @@
-import React from "react";
+import { useState } from "react";
 import { motion } from "framer-motion";
 
 const test = () => {
+  const [open, setOpen] = useState(false);
+
   const variants = {
-    visible: { opacity: 1 },
+    visible: {
+      opacity: 1,
+      // x: 100,
+      transition: { staggerChildren: 0.2 },
+    },
     hidden: { opacity: 0 },
   };
+
+  const items = ["item1", "item2", "item3", "item4"];
+
   return (
     <div className="course">
-      <motion.div
-        className="box"
-        variants={variants}
-        initial="hidden"
-        animate="visible"
-        transition={{ duration: 4 }}
-        drag
-      ></motion.div>
+      <motion.ul initial="hidden" animate="visible" variants={variants}>
+        {items.map((item) => (
+          <motion.li variants={variants} key={item}>
+            {item}
+          </motion.li>
+        ))}
+      </motion.ul>
     </div>
   );
 };
