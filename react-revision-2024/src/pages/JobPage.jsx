5c9b45157218dcdc76a2363fd88ad6b03b5a68e0
Saqlain Javed <saqlainsj12@gmail.com>
2024-10-19 01:25:43 +0500

    fetchAPI - Delete method

--- react-revision-2024/src/pages/JobPage.jsx
+++ react-revision-2024/src/pages/JobPage.jsx
@@ -1,10 +1,10 @@
 // import { useState, useEffect } from "react";
 // import Spinner from "../components/Spinner";
-import { useParams, useLoaderData } from "react-router-dom";
+import { useParams, useLoaderData, useNavigate } from "react-router-dom";
 import { FaArrowLeft, FaMapMarker } from "react-icons/fa";
 import { Link } from "react-router-dom";
 
-const JobPage = () => {
+const JobPage = ({ deleteJob }) => {
   //   const { id } = useParams();
   //   const [job, setJob] = useState(null);
   //   const [loading, setLoading] = useState(true);
@@ -28,6 +28,20 @@ const JobPage = () => {
 
   // loader function
   const job = useLoaderData();
+
+  const navigate = useNavigate();
+
+  const onDeleteClick = (jobId) => {
+    const confirm = window.confirm(
+      "Are you sure you want to delete this listing?"
+    );
+
+    if (!confirm) return;
+
+    deleteJob(jobId);
+
+    navigate("/jobs");
+  };
   return (
     <>
       <section>
@@ -103,7 +117,10 @@ const JobPage = () => {
                 >
                   Edit Job
                 </Link>
-                <button className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block">
+                <button
+                  onClick={() => onDeleteClick(job.id)}
+                  className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline mt-4 block"
+                >
                   Delete Job
                 </button>
               </div>
