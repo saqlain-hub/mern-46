54051658e6f7492cbfc6ebe3e2c92f8dcde60b38
Saqlain Javed <saqlainsj12@gmail.com>
2024-11-05 01:37:48 +0500

    Middleware - Logger and Error handler

--- express/src/server.js
+++ express/src/server.js
@@ -1,6 +1,9 @@
 import express from "express";
 import path from "path";
 import posts from "./routes/posts.js";
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/error.js";
+
 const port = process.env.PORT || 8000;
 
 const app = express();
@@ -9,10 +12,16 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+// Logger middleware
+app.use(logger);
+
 // Setup static folder
 // app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/api/posts", posts);
+
+// Error handler
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
