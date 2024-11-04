615b1840252734bf7ecb42ef4c93c3f1d63bdfaa
Saqlain Javed <saqlainsj12@gmail.com>
2024-11-05 02:26:59 +0500

    setting up the static server with public folder without commonjs

--- express/src/server.js
+++ express/src/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import path from "path";
+import { fileURLToPath } from "url";
 import posts from "./routes/posts.js";
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/error.js";
 import notFound from "./middleware/notFound.js";
 
 const port = process.env.PORT || 8000;
+
+// Get the directory name
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 
@@ -17,7 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(logger);
 
 // Setup static folder
-// app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "public")));
 
 //Routes
 app.use("/api/posts", posts);
